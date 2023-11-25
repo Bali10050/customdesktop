f0917478a1843847d3d63d26a560f08d3e3bb8ee
Séra Balázs <serabali10050@gmail.com>
2023-11-25 12:59:20 +0100

    Add files via upload

---
+++ user.js
@@ -0,0 +1,13 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.tabbox.switchByScrolling", true);
+user_pref("browser.tabs.searchclipboardfor.middleclick", false);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.rdd-ffmpeg.enabled", true);
+user_pref("widget.dmabuf.force-enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("media.av1.enabled", false);
+user_pref("gfx.x11-egl.force-enabled", false);
+user_pref("layout.frame_rate", 144);
+user_pref("network.http.max-connections", 255);
+user_pref("network.http.max-persistent-connections-per-server", 30);
